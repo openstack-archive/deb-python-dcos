785b548386ace44c1922d3fd70addc4c85b3c305
Karl Isenberg <karl@mesosphere.io>
2017-05-11 13:45:38 -0500

    Clean up install script

--- bin/install/install-binary-dcos-cli.sh
+++ bin/install/install-binary-dcos-cli.sh
@@ -31,7 +31,7 @@ function usage() {
 # Query the DC/OS API for the DC/OS version
 function detect_dcos_version() {
   # Auto-detect version (unauthenticated)
-  DCOS_VERSION_JSON="$(curl --fail --location --silent --show-error ${DCOS_URL%/}/dcos-metadata/dcos-version.json)"
+  DCOS_VERSION_JSON="$(curl --insecure --fail --location --silent --show-error ${DCOS_URL%/}/dcos-metadata/dcos-version.json)"
   # Extract version from metadata
   # Warning: requires json to be pretty-printed with line breaks
   # Full json parsing would require a dependency like python or jq.
@@ -80,11 +80,8 @@ INSTALL_DIR_PATH="${2:-}" #optional
 DCOS_VERSION="$(detect_dcos_version)"
 echo >&2 "DC/OS Version: ${DCOS_VERSION}"
 
-# Get major version by stripping the last version segment
-VERSION_PATTERN='[^0-9]*\([0-9]*\)[.]\([0-9]*\).*'
-MAJOR_VERSION="$(echo "${DCOS_VERSION}" | sed -e "s#${VERSION_PATTERN}#\1#")"
-MINOR_VERSION="$(echo "${DCOS_VERSION}" | sed -e "s#${VERSION_PATTERN}#\2#")"
-DCOS_MAJOR_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}"
+# Get major version by extracting the first two segments
+DCOS_MAJOR_VERSION="$(echo "${DCOS_VERSION}" | sed -e "s#[^0-9]*\([0-9][0-9]*[.][0-9][0-9]*\).*#\1#")"
 echo >&2 "DC/OS Major Version: ${DCOS_MAJOR_VERSION}"
 
 case "${OSTYPE}" in
@@ -99,9 +96,12 @@ INSTALL_DIR_PATH="${INSTALL_DIR_PATH:-${BIN}}"
 CLI_DIR="$(create_temp_dir)"
 trap "rm -rf ${CLI_DIR}" EXIT
 
+echo >&2 "Downloading..."
 DOWNLOAD_PATH="$(download_cli "${CLI_DIR}")"
+echo >&2 "Installing..."
 INSTALL_PATH="$(install_cli "${DOWNLOAD_PATH}" "${INSTALL_DIR_PATH}")"
 
+echo >&2 "Configuring..."
 echo >&2 "Config: core.dcos_url=${DCOS_URL}"
 dcos config set core.dcos_url "${DCOS_URL}"
 
