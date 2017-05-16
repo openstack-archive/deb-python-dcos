0abf9f6077ead3a13910a7e2a6c614b1093be875
Karl Isenberg <karl@mesosphere.io>
2017-05-16 15:04:28 -0700

    Refactor install-binary-dcos-cli.sh
    - Remove configuration at the end (different for each version)
    - Add DCOS_VERSION env var option
    - Move install path to DCOS_VERSION env var option
    - extract several methods to make the execution apth more clear
    - Distinguish install path (directory) from executable path (binary)

--- bin/install/install-binary-dcos-cli.sh
+++ bin/install/install-binary-dcos-cli.sh
@@ -1,31 +1,38 @@
 #!/usr/bin/env bash
 
-# Installs and configures the binary DC/OS CLI.
+# Installs the binary DC/OS CLI.
 #
 # Auto-detects the DC/OS version by asking an existing DC/OS cluster.
 # Auto-detects the local operating system to download the right DC/OS CLI binary.
 #
-# Arguments:
-#   dcos-url -- (required) URL to DC/OS cluster (default: http://m1.dcos/)
-#   install-dir-path -- (optional) Directory path to install into (default: platform dependent)
+# Arguments (required):
+#   dcos-url -- URL to DC/OS cluster (ex: http://m1.dcos/)
+#
+# Variables (optional):
+#   DCOS_VERSION -- Version of DC/OS the CLI should be compatible with (default: query from dcos-url)
+#   INSTALL_PATH -- Directory path to install into (default: platform dependent)
 #
 # Usage:
-# $ bin/install/dcos-binary-install-cli.sh <dcos-url> [install-dir-path]
+# $ install-binary-dcos-cli.sh <dcos-url>
 # $ dcos --version
 #
-# Alt Usage:
-# $ EXE="$(ci/dcos-binary-install-cli.sh <dcos-url> [install-dir-path] 2>/dev/null)"
+# Save executable location:
+# $ EXE="$(install-binary-dcos-cli.sh <dcos-url> 2>/dev/null)"
 # $ ${EXE} --version
 #
+# Specify DC/OS version (no cluster required):
+# $ DCOS_VERSION=1.10.0 install-binary-dcos-cli.sh <dcos-url>
+# $ dcos --version
+#
 # Remote Usage:
-# $ curl https://downloads.dcos.io/dcos-cli/bin/install/dcos-binary-install-cli.sh | bash -s <dcos-url> [install-dir-path]
+# $ curl https://downloads.dcos.io/dcos-cli/bin/install/install-binary-dcos-cli.sh | bash -s <dcos-url>
 # $ dcos --version
 
 set -o errexit -o nounset -o pipefail
 
 # Show usage information.
 function usage() {
-  echo "$(basename "$(test -L "$0" && readlink "$0" || echo "$0")") <dcos-url> [install-dir-path]"
+  echo "$(basename "$(test -L "$0" && readlink "$0" || echo "$0")") <dcos-url>"
 }
 
 # Query the DC/OS API for the DC/OS version
@@ -41,54 +48,76 @@ function detect_dcos_version() {
 
 function create_temp_dir() {
   TMPDIR="${TMPDIR:-/tmp/}"
-  CLI_DIR="$(mktemp -d "${TMPDIR%/}/dcos-install-cli.XXXXXXXXXXXX")"
-  echo "${CLI_DIR}"
+  echo "$(mktemp -d "${TMPDIR%/}/dcos-install-cli.XXXXXXXXXXXX")"
 }
 
 function download_cli() {
-  CLI_DIR="${1}"
-  DCOS_CLI_URL="https://downloads.dcos.io/binaries/cli/${PLATFORM}/dcos-${DCOS_MAJOR_VERSION}/${EXE}"
+  DCOS_VERSION="${1}"
+  DOWNLOAD_PATH="${2}"
+  PLATFORM="$(detect_platform)"
+  DCOS_MAJOR_VERSION="$(dcos_major_version "${DCOS_VERSION}")"
+  EXE_NAME="$(detect_exe_name)"
+  DCOS_CLI_URL="https://downloads.dcos.io/binaries/cli/${PLATFORM}/dcos-${DCOS_MAJOR_VERSION}/${EXE_NAME}"
   echo >&2 "Download URL: ${DCOS_CLI_URL}"
-  echo >&2 "Download Path: ${CLI_DIR}/${EXE}"
-  curl --fail --location --silent --show-error -o "${CLI_DIR}/${EXE}" "${DCOS_CLI_URL}"
-  echo "${CLI_DIR}/${EXE}"
+  echo >&2 "Download Path: ${DOWNLOAD_PATH}/${EXE_NAME}"
+  echo >&2 "Downloading..."
+  curl --fail --location --silent --show-error -o "${DOWNLOAD_PATH}/${EXE_NAME}" "${DCOS_CLI_URL}"
+  echo "${DOWNLOAD_PATH}/${EXE_NAME}"
 }
 
 function install_cli() {
   DOWNLOAD_PATH="${1}"
-  INSTALL_DIR_PATH="${2}"
-  INSTALL_PATH="${INSTALL_DIR_PATH}/$(basename "${DOWNLOAD_PATH}")"
+  INSTALL_PATH="${2}"
+  echo >&2 "Installing..."
+  EXE_PATH="${INSTALL_PATH}/$(basename "${DOWNLOAD_PATH}")"
   chmod a+x "${DOWNLOAD_PATH}"
   # only use sudo if required
-  if [[ -w "${INSTALL_DIR_PATH}" ]]; then
-    mv "${DOWNLOAD_PATH}" "${INSTALL_PATH}"
+  if [[ -w "${INSTALL_PATH}" ]]; then
+    mv "${DOWNLOAD_PATH}" "${EXE_PATH}"
   else
-    sudo mv "${DOWNLOAD_PATH}" "${INSTALL_PATH}"
+    sudo mv "${DOWNLOAD_PATH}" "${EXE_PATH}"
   fi
-  echo "${INSTALL_PATH}"
+  echo "${EXE_PATH}"
 }
 
 function detect_cli_version() {
   dcos --version | grep dcoscli.version | cut -d '=' -f 2
 }
 
-# return 0 if version A >= version B using semanatic versioning
-function semver_gte() {
-  VERISON_A="${1}"
-  VERISON_B="${2}"
-  SEMVER_PATTERN="[^0-9]*\([0-9][0-9]*\)[.]\([0-9][0-9]*\)[.]\([0-9][0-9]*\).*"
-  SEG1_A="$(echo "${VERISON_A}" | sed -e "s#${SEMVER_PATTERN}#\1#")"
-  SEG1_B="$(echo "${VERISON_B}" | sed -e "s#${SEMVER_PATTERN}#\1#")"
-  [[ ${SEG1_A} < ${SEG1_B} ]] && return 1
-  [[ ${SEG1_A} > ${SEG1_B} ]] && return 0
-  SEG2_A="$(echo "${VERISON_A}" | sed -e "s#${SEMVER_PATTERN}#\2#")"
-  SEG2_B="$(echo "${VERISON_B}" | sed -e "s#${SEMVER_PATTERN}#\2#")"
-  [[ ${SEG2_A} < ${SEG2_B} ]] && return 1
-  [[ ${SEG2_A} > ${SEG2_B} ]] && return 0
-  SEG3_A="$(echo "${VERISON_A}" | sed -e "s#${SEMVER_PATTERN}#\3#")"
-  SEG3_B="$(echo "${VERISON_B}" | sed -e "s#${SEMVER_PATTERN}#\3#")"
-  [[ ${SEG3_A} < ${SEG3_B} ]] && return 1
-  return 0
+function detect_platform() {
+  case "${OSTYPE}" in
+    darwin*)  PLATFORM='darwin/x86-64' ;;
+    linux*)   PLATFORM='linux/x86-64' ;;
+    msys*)    PLATFORM='windows/x86-64' ;;
+    *)        echo >&2 "Unsupported operating system: ${OSTYPE}"; exit 1 ;;
+  esac
+  echo "${PLATFORM}"
+}
+
+function detect_install_path() {
+  case "${OSTYPE}" in
+    darwin*)  INSTALL_PATH='/usr/local/bin' ;;
+    linux*)   INSTALL_PATH='/usr/local/bin' ;;
+    msys*)    INSTALL_PATH="${HOME}/AppData/Local/Microsoft/WindowsApps" ;;
+    *)        echo >&2 "Unsupported operating system: ${OSTYPE}"; exit 1 ;;
+  esac
+  echo "${INSTALL_PATH}"
+}
+
+function detect_exe_name() {
+  case "${OSTYPE}" in
+    darwin*)  EXE_NAME='dcos' ;;
+    linux*)   EXE_NAME='dcos' ;;
+    msys*)    EXE_NAME='dcos.exe' ;;
+    *)        echo >&2 "Unsupported operating system: ${OSTYPE}"; exit 1 ;;
+  esac
+  echo "${EXE_NAME}"
+}
+
+# Convert from DC/OS version to "major" version used by CLI URL
+function dcos_major_version() {
+  DCOS_VERSION="${1}"
+  echo "$(echo "${DCOS_VERSION}" | sed -e "s#[^0-9]*\([0-9][0-9]*[.][0-9][0-9]*\).*#\1#")"
 }
 
 if [[ "$#" -lt 1 ]]; then
@@ -96,46 +125,34 @@ if [[ "$#" -lt 1 ]]; then
   exit 1;
 fi
 
-DCOS_URL="${1}" #required
+# DC/OS URL (required)
+DCOS_URL="${1}"
 echo >&2 "DC/OS URL: ${DCOS_URL}"
-INSTALL_DIR_PATH="${2:-}" #optional
 
-DCOS_VERSION="$(detect_dcos_version)"
+# DC/OS Version (optional)
+if [[ -z "${DCOS_VERSION:-}" ]]; then
+  # auto-detect version, if not provided
+  DCOS_VERSION="$(detect_dcos_version)"
+fi
 echo >&2 "DC/OS Version: ${DCOS_VERSION}"
 
-# Get major version by extracting the first two segments
-DCOS_MAJOR_VERSION="$(echo "${DCOS_VERSION}" | sed -e "s#[^0-9]*\([0-9][0-9]*[.][0-9][0-9]*\).*#\1#")"
-echo >&2 "DC/OS Major Version: ${DCOS_MAJOR_VERSION}"
-
-case "${OSTYPE}" in
-  darwin*)  PLATFORM='darwin/x86-64'; BIN='/usr/local/bin'; EXE='dcos' ;;
-  linux*)   PLATFORM='linux/x86-64'; BIN='/usr/local/bin'; EXE='dcos' ;;
-  msys*)    PLATFORM='windows/x86-64'; BIN="${HOME}/AppData/Local/Microsoft/WindowsApps"; EXE='dcos.exe' ;;
-  *)        echo >&2 "Unsupported operating system: ${OSTYPE}"; exit 1 ;;
-esac
-INSTALL_DIR_PATH="${INSTALL_DIR_PATH:-${BIN}}"
+# Install Path (optional)
+if [[ -z "${INSTALL_PATH:-}" ]]; then
+  # auto-detect install path, if not provided
+  INSTALL_PATH="$(detect_install_path)"
+fi
+echo >&2 "Install Path: ${INSTALL_PATH}"
 
 # Use temp dir to download into before install, delete on script exit
-CLI_DIR="$(create_temp_dir)"
-trap "rm -rf ${CLI_DIR}" EXIT
+DOWNLOAD_PATH="$(create_temp_dir)"
+trap "rm -rf ${DOWNLOAD_PATH}" EXIT
 
-echo >&2 "Downloading..."
-DOWNLOAD_PATH="$(download_cli "${CLI_DIR}")"
-echo >&2 "Installing..."
-INSTALL_PATH="$(install_cli "${DOWNLOAD_PATH}" "${INSTALL_DIR_PATH}")"
+EXE_PATH="$(download_cli "${DCOS_VERSION}" "${DOWNLOAD_PATH}")"
+EXE_PATH="$(install_cli "${EXE_PATH}" "${INSTALL_PATH}")"
 
 CLI_VERSION="$(detect_cli_version)"
 echo >&2 "CLI Version: ${CLI_VERSION}"
 
-echo >&2 "Configuring CLI..."
-if semver_gte "${CLI_VERSION}" "0.5.0"; then
-  echo >&2 "dcos cluster setup ${DCOS_URL}"
-  dcos cluster setup "${DCOS_URL}"
-else
-  echo >&2 "dcos config set core.dcos_url ${DCOS_URL}"
-  dcos config set core.dcos_url "${DCOS_URL}"
-fi
-
 # Print install path to STDOUT to enable script chaining
-echo >&2 "Install Path:"
-echo "${INSTALL_PATH}"
+echo >&2 "Executable Path:"
+echo "${EXE_PATH}"
